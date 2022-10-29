8fe525adc0da207972f3ab35a243e5a269f6181f
Eran Assaf <assaferan@gmail.com>
2022-10-29 12:05:27 -0400

    a script for collecting the results of hecke_runs

---
+++ hecke_collect.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+fnames = ["data/hecke_"+str(i) + ".dat" for i in [1..999]]
+hecke_strs = [open(f).read() for f in fnames]
+hecke_str = "[{}," + ",".join(hecke_strs) + "]"
+# not really needed, but we want to check validity
+hecke = eval(hecke_str)
+out_fname = "data/hecke_all.dat"
+f = open(out_fname, "w")
+f.write(hecke_str)
+f.close()
