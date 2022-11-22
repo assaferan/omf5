64658cac07ebf144835b9d51e0e35f93d114d9ec
Eran Assaf <assaferan@gmail.com>
2022-11-22 13:48:09 -0500

    updating running script

--- scripts/hecke_run.py
+++ scripts/hecke_run.py
@@ -14,7 +14,7 @@ discs = range(1,MAX_DISC)
 
 # full hecke matrix T_p for p up to 10
 ps = {d : [p for p in prime_range(MAX_P_FULL) if (d % p != 0)] for d in discs}
-cmds = [["./src/omf5 -genus=data/qf5.db -disc=%d -hecke -p=%d > data/hecke_mat_%d_%d.dat 2> logs/hecke_mat_%d_%d.log &" %(d, p, d, p, d, p) for p in ps[d] ] for d in discs]
+cmds = [["./src/omf5 -genus=data/qf5db.sage -isom -disc=%d -hecke -p=%d > data/hecke_mat_%d_%d.dat 2> logs/hecke_mat_%d_%d.log &" %(d, p, d, p, d, p) for p in ps[d] ] for d in discs]
 cmds = reduce(lambda x,y : x+y, cmds)
 for cmd in cmds:
     print "Executing %s" %(cmd)
@@ -22,7 +22,7 @@ for cmd in cmds:
 
 # first row of hecke matrix T_p for p up to 100
 ps = {d : [p for p in prime_range(MAX_P_ROW) if (d % p != 0)] for d in discs}
-cmds = [["./src/omf5 -genus=data/qf5.db -disc=%d -hecke -p=%d -row > data/hecke_row_%d_%d.dat 2> logs/hecke_row_%d_%d.log &" %(d, p, d, p, d, p) for p in ps[d] ] for d in discs]
+cmds = [["./src/omf5 -genus=data/qf5db.sage -isom -disc=%d -hecke -p=%d -row > data/hecke_row_%d_%d.dat 2> logs/hecke_row_%d_%d.log &" %(d, p, d, p, d, p) for p in ps[d] ] for d in discs]
 cmds = reduce(lambda x,y : x+y, cmds)
 for cmd in cmds:
     print "Executing %s" %(cmd)
