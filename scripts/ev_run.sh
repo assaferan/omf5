c27df99ae23160af04bd25a12b4e0b87764e9dec
assaferan <assaferan@gmail.com>
2022-11-29 11:40:36 -0500

    run to scale

--- scripts/ev_run.sh
+++ scripts/ev_run.sh
@@ -1,6 +1,6 @@
 export LD_LIBRARY_PATH=/home/assaferan/lib
 
-for i in {1..9}
+for i in {1..999}
 do
 ./src/omf5 -genus=data/qf5db.sage -isom -disc=$i -prec=100 > data/hecke_ev_3_0_$i.dat 2> logs/hecke_ev_3_0_$i.log &
 done
