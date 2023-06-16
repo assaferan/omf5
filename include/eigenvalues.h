ee78622c65b2d364f4bc172e6a5b5e6ceafc5411
assaferan <assaferan@gmail.com>
2023-06-16 18:12:54 -0400

    updated documentation

--- include/eigenvalues.h
+++ include/eigenvalues.h
@@ -1,5 +1,18 @@
+/**********************************************************
+ *
+ * Package : omf5 - orthogonal modular forms of rank 5
+ * Filename : eigenvalues.h
+ *
+ * Description: data of eigenforms and their eigenvalues
+ *              for a space of orthogonal modular forms.
+ *
+ **********************************************************
+ */
+
 #ifndef __EIGENVALUES_H__
 #define __EIGENVALUES_H__
+
+// Required packages dependencies
 
 #include <antic/nf.h>
 #include <antic/nf_elem.h>
@@ -8,16 +21,59 @@
 
 #include <flint/fmpz.h>
 
+// Self dependencies
+
 #include "genus.h"
 #include "typedefs.h"
+
+/****************************************************************
+ *
+ * Type: aut_type
+ *
+ * Description: automorphic type of an eigenform.
+ *
+ * An eigenform f gives rise to a automorphic representation pi,
+ * whose type is one of F (Eisenstein), P (Saito-Kurokawa),
+ * Y (Yoshida), or G (in the orthogonal complement of the above).
+ * We added the type O to indicate oldforms.
+ *
+ ****************************************************************
+ */
 
 typedef enum {
   G,
   Y, // I thought we were not supposed to see Yoshida lifts, because paramodular ?
+  // we see them in the spaces of orthogonal modular forms, but they do not correspond to paramodular forms
   P,
   F,
   O // oldform
 } aut_type;
+
+/****************************************************************
+ *
+ * Type: eigenvalues_t
+ *
+ * Description: this struct holds data for the eigenforms
+ *              and their eigenvalues at a specific prime. 
+ *
+ * Fields:
+ *     + nfs (nf_t*) - a list of the number fields K(f), over
+ *                     which the Hecke eigenvalues of f are
+ *                     defined
+ *     + eigenvals (nf_elem_t*) - a list of Hecke eigenvalues,
+ *                                storing the eigenvalue of a
+ *                                single Hecke operator
+ *     + eigenvecs (nf_elem_t**) - a list of vectors,
+ *                                 representing the eigenforms
+ *                                 in the basis of indicator
+ *                                 functions for the genus
+ *     + num (slong) - number of eigenforms
+ *     + dim (slong) - dimension of the space
+ *     + lift_type (aut_type*) - automorphic type of the form
+ *
+ * Used to hold the data of eigenforms and their eigenvalues,
+ ****************************************************************
+ */
 
 typedef struct {
   nf_t* nfs;
@@ -31,18 +87,116 @@ typedef struct {
 
 typedef eigenvalues eigenvalues_t[1];
 
+/****************************************************************
+ *
+ * Type: all_eigen
+ *
+ * Description: Data for all eigenvalues.
+ *
+ * Fields:
+ *     + evs (eigenvalues_t*) - a list of eigenvalue structures,
+ *                              one for each prime
+ *     + num (slong) - number of eigenvalues
+ *
+ ****************************************************************
+ */
+
 typedef struct {
   eigenvalues_t* evs;
   slong num;
 } all_eigen;
 
+/******************************************************************
+ *
+ * Function: eigenvalues_init
+ *
+ * Description: Allocates memory for an eigenvalue struct.
+ *
+ * Arguments:
+ *     + num (slong) - number of eigenvectors
+ *     + dim (slong) - dimension of the space (length of vectors)
+ *
+ * Returns:
+ *     + evs (eigenvalues_t) - the empty eigenvalues data
+ *
+ ******************************************************************
+ */
+
 void eigenvalues_init(eigenvalues_t evs, slong num, slong dim);
+
+/*********************************************************************
+ *
+ * Function: eigenvalues_init_set_mat
+ *
+ * Description: Get eigenvectors and eigenvalues of a matrix.
+ *
+ * Arguments:
+ *     + mat (matrix_TYP*) - the matrix (CARAT type)
+ *
+ * Returns:
+ *     + evs (eigenvalues_t) - the eigenvectors and eigenvalues data
+ *
+ *********************************************************************
+ */
 
 void eigenvalues_init_set_mat(eigenvalues_t evs, matrix_TYP* mat);
 
+/*********************************************************************
+ *
+ * Function: eigenvalues_set_lifts
+ *
+ * Description: Find which eigenforms are lifts,
+ *              and set the automorphic types accordingly.
+ *              Done by considering factorization of L-polynomials.
+ *
+ * Arguments:
+ *     + evs (eigenvalues_t) - the eigenvectors
+ *     + prec (slong) - number of primes at which to test the L-poly
+ *     + c (slong) - the conductor
+ *     + genus (const genus_t) - the genus 
+ *
+ * Returns:
+ *     + evs (eigenvalues_t) - the eigenvectors with the aut_type set
+ *
+ *********************************************************************
+ */
+
 void eigenvalues_set_lifts(eigenvalues_t evs, slong prec, slong c, const genus_t genus);
 
+/*********************************************************************
+ *
+ * Function: ev_get_pivot
+ *
+ * Description: Find a nonzero entry in the vector
+ *              representing the eigenform, such that the
+ *              corresponding lattice in the genus has a
+ *              maximal number of automorphisms.
+ *
+ * Arguments:
+ *     + evec (const nf_elem_t*) - the eigenvector representing f
+ *     + K (nf_t) - the number field K(f)
+ *     + genus (const genus_t) - the genus 
+ *     + cond (slong) - the conductor
+ *
+ * Returns:
+ *     + (slong) - index of the entry.
+ *
+ *********************************************************************
+ */
+
 slong ev_get_pivot(const nf_elem_t* evec, const nf_t K, const genus_t genus, slong cond);
+
+/******************************************************************
+ *
+ * Function: eigenvalues_clear
+ *
+ * Description: Deallocates memory for an eigenvalues struct.
+ *
+ * Arguments:
+ *     + evs (eigenvalues_t) - the eigenvalues data
+ *
+ ******************************************************************
+ */
 
 void eigenvalues_clear(eigenvalues_t evs);
 
--- src/eigenvalues.c
+++ src/eigenvalues.c
@@ -1,4 +1,19 @@
+/**********************************************************
+ *
+ * Package : omf5 - orthogonal modular forms of rank 5
+ * Filename : eigenvalues.c
+ *
+ * Description: Compute eigenforms and their eigenvalues
+ *              in spaces of orthogonal modular forms.
+ *
+ **********************************************************
+ */
+
+// System dependencies
+
 #include <assert.h>
+
+// Required packages dependencies
 
 #include <flint/fmpz.h>
 #include <flint/fmpz_mat.h>
@@ -6,11 +21,14 @@
 #include <flint/fmpz_poly_factor.h>
 #include <flint/fmpq_poly.h>
 
+// Self dependencies
+
 #include "eigenvalues.h"
 #include "fmpq_poly.h"
 #include "hecke.h"
 #include "nf_mat.h"
 
+// initialize the eigenvalues struct - allocate according memory
 void eigenvalues_init(eigenvalues_t evs, slong num, slong dim)
 {
   slong i;
@@ -27,6 +45,7 @@ void eigenvalues_init(eigenvalues_t evs, slong num, slong dim)
   return;
 }
 
+// compute eigenvecotrs and eigenvalues for the matrix mat
 void eigenvalues_init_set_mat(eigenvalues_t evs, matrix_TYP* mat)
 {
   fmpz_mat_t M;
@@ -35,13 +54,17 @@ void eigenvalues_init_set_mat(eigenvalues_t evs, matrix_TYP* mat)
   fmpq_poly_t nf_poly;
   int row, col, i, j;
   nf_mat_t M_K, ker, lambda;
-  
+
+  // start by creating an fmpz_mat matrix from mat
+  // !! TODO - maybe we now have a function that does that,
+  // and we can replace the following lines by a function?
   fmpz_mat_init(M, mat->rows, mat->cols);
 
   for (row = 0; row < mat->rows; row++)
     for (col = 0; col  < mat->cols; col++)
       *fmpz_mat_entry(M, row, col) = mat->array.SZ[row][col];
 
+  // We then factor the characteristic polynomial
   fmpz_poly_init(cp);
   fmpz_mat_charpoly(cp, M);
 #ifdef DEBUG_LEVEL_FULL
@@ -58,25 +81,37 @@ void eigenvalues_init_set_mat(eigenvalues_t evs, matrix_TYP* mat)
   }
 #endif // DEBUG_LEVEL_FULL
 
+  // and initialize the eigenvalues data according to the number of factors
+  // (we only save one for each Galois orbit)
   eigenvalues_init(evs, cp_fac->num, mat->rows);
   
   for (i = 0; i < cp_fac->num; i++) {
+    // We create a number field , K, defined by the irreducible factor
     fmpq_poly_init(nf_poly);
     fmpq_poly_set_fmpz_poly(nf_poly, &(cp_fac->p[i]));
     nf_init(evs->nfs[i], nf_poly);
+    // and set the eigenvalue to be the generator of the number field,
+    // i.e. a root of this polynomial
     nf_elem_init(evs->eigenvals[i], evs->nfs[i]);
     nf_elem_gen(evs->eigenvals[i], evs->nfs[i]);
+    // We now proceed to compute the eigenvector.
+    // For that we need to base change our matrix to K
     nf_mat_init(M_K, &(evs->nfs[i]), mat->rows, mat->cols);
     nf_mat_init(lambda, &(evs->nfs[i]), mat->rows, mat->cols);
     fmpz_mat_get_nf_mat(M_K, M);
+    // lambda is the scalar matrix whose value is our eigenvalue 
     nf_mat_one(lambda);
     nf_mat_scalar_mul_nf(lambda, lambda, evs->eigenvals[i]);
     nf_mat_sub(M_K, M_K, lambda);
+    // Kernel(M - lambda I) is the eigenspace
     nf_mat_kernel(ker, M_K);
 #ifdef DEBUG
     printf("ker = \n");
     nf_mat_print_pretty(ker);
 #endif // DEBUG
+    // We add all basis vectors as eigenvectors
+    // When we have multiplicity one, these will always be one dimensional,
+    // and we will obtain the unique eigenvector with eigenvalue lambda.
     for (j = 0; j < mat->rows; j++) {
       nf_elem_init(evs->eigenvecs[i][j], evs->nfs[i]);
       nf_elem_set(evs->eigenvecs[i][j], *nf_mat_entry(ker, 0, j), evs->nfs[i]);
@@ -93,11 +128,13 @@ void eigenvalues_init_set_mat(eigenvalues_t evs, matrix_TYP* mat)
   return;
 }
 
+// Clear the memory allocated for eigenvalues data
 void eigenvalues_clear(eigenvalues_t evs)
 {
   int i, j;
 
   for (i = 0; i < evs->num; i++) {
+    // If our form is an oldform, then we have not computed the eigenvalues since they might be meaningless
     if (evs->lift_type[i] == O)
       continue;
     for (j = 0; j < evs->dim; j++) {
@@ -116,33 +153,37 @@ void eigenvalues_clear(eigenvalues_t evs)
   return;
 }
 
+// Check if an element of a number field is a square
+// !! TODO - this should be public and in a different file
+// returns true if x is a square in K
 bool nf_elem_is_square(const nf_elem_t x, const nf_t K)
 {
   // we do it in a weird way because of the lack of functionality of nf_elem
-
-  slong i;
+  // Based on S. Landau, Factoring Polynomials over Algebraic Number Fields, SIAM Journal on Computing, 1985.
+  // where we simply try to factor t^2 - x
+  
   fmpq_mat_t x_reg;
   fmpq_poly_t min_poly, x2;
   fmpq_poly_factor_t fac;
   bool is_sqr = false;
 
+  // This sets the polynomial t^2
   fmpq_poly_init(x2);
   fmpq_poly_init(min_poly);
   fmpq_mat_init(x_reg, fmpq_poly_degree(K->pol), fmpq_poly_degree(K->pol));
   fmpq_poly_set_str(x2, "3  0 0 1");
-  
+
+  // x_reg is the matrix representing multiplication by x
+  // If we let f(t) be its minimal polynomial,
+  // then we factor f(t^2)
+  // Note that f(t^2) = prod_i (t^2 - x_i) = N(t^2 - x)
   nf_elem_rep_mat(x_reg, x, K);
   fmpq_mat_minpoly(min_poly, x_reg);
   fmpq_poly_compose(min_poly, min_poly, x2);
   fmpq_poly_factor(fac, min_poly);
-
-  for (i = 0; i < fac->num; i++) {
-    if (fac->num > 1) { // Theorem 1.4 from Landau's paper
-      is_sqr = true;
-      break;
-    }
-  }
-
+  
+  is_sqr =  (fac->num > 1); // Theorem 1.4 from Landau's paper
+  
   fmpq_poly_factor_clear(fac);
   fmpq_poly_clear(x2);
   fmpq_poly_clear(min_poly);
@@ -151,6 +192,11 @@ bool nf_elem_is_square(const nf_elem_t x, const nf_t K)
   return is_sqr;
 }
 
+// Check if an element of a number field is a square
+// !! TODO - this should be public and in a different file
+// returns true if x is a square in K
+// The above function might be occassionaly slow.
+// This is a faster approach, based on CRT
 bool nf_elem_is_square_fast(const nf_elem_t x, const nf_t K)
 {
   // we do it in a weird way because of the lack of functionality of nf_elem
@@ -179,6 +225,7 @@ bool nf_elem_is_square_fast(const nf_elem_t x, const nf_t K)
   fmpz_mul(den, den, den);
   nf_elem_scalar_mul_fmpz(x_int, x, den, K);
 
+  // bound is the maximal absolute value of a coefficient in the defining polynomial
   fmpz_one(bound);
   for (i = 0; i < n; i ++) {
     nf_elem_get_coeff_fmpz(coeff, x_int, i, K);
@@ -189,6 +236,7 @@ bool nf_elem_is_square_fast(const nf_elem_t x, const nf_t K)
   fmpz_mul(bound, bound, bound);
   // bound^2 is not enough!! Consider 21/2*(7 + sqrt(13))
   fmpz_mul(bound, bound, bound);
+  // Are we sure that bound^3 is enough?
   fmpz_init(prod);
   fmpz_one(prod);
 
@@ -199,8 +247,11 @@ bool nf_elem_is_square_fast(const nf_elem_t x, const nf_t K)
     fmpz_nextprime(p, p, true);
     fmpz_mul(prod, prod, p);
     p_int = fmpz_get_si(p);
+    // We now want to check whether it is square in each of the extensions
+    // of our number field over p
     fmpq_poly_get_numerator(f_int, K->pol);
     nmod_poly_init2(f_int_mod_p, p_int, n+1);
+    // Construct the defining polynomial with coefficients in F_p
     for (i = 0; i <= n; i++) {
       fmpz_poly_get_coeff_fmpz(coeff, f_int, i);
       fmpz_mod_ui(coeff, coeff, p_int);
@@ -212,7 +263,8 @@ bool nf_elem_is_square_fast(const nf_elem_t x, const nf_t K)
       */
       nmod_poly_set_coeff_ui(f_int_mod_p, i, coeff_int);
     }
-    
+
+    // Construct the polynomial represeting the element x over F_p
     nmod_poly_init2(g_int_mod_p, p_int, n);
     for (i = 0; i < n; i++) {
       nf_elem_get_coeff_fmpz(coeff, x_int, i, K);
@@ -226,13 +278,17 @@ bool nf_elem_is_square_fast(const nf_elem_t x, const nf_t K)
       nmod_poly_set_coeff_ui(g_int_mod_p, i, coeff_int);
     }
 
+    // Factor f_int_mod_p to construct the different extensions of F_p
     nmod_poly_factor(fac, f_int_mod_p);
     for (i = 0; i < fac->num; i++) {
+      // Find the image of x in this finite field
       nmod_poly_init(r_int_mod_p, p_int);
       nmod_poly_rem(r_int_mod_p, g_int_mod_p, &(fac->p[i]));
+      // construct the extension F = F_q 
       fq_nmod_ctx_init_modulus(F, &(fac->p[i]), "x");
       fq_nmod_init(r, F);
       fq_nmod_set(r, r_int_mod_p, F);
+      // If x is not a square in F, then it is not a square. 
       if (!fq_nmod_is_square(r, F)) {
 	fq_nmod_clear(r, F);
 	fq_nmod_ctx_clear(F);
@@ -263,10 +319,18 @@ bool nf_elem_is_square_fast(const nf_elem_t x, const nf_t K)
   fmpz_clear(p);
   fmpz_clear(coeff);
   fmpz_clear(den);
+
+  // If we are here, x is a square in K_P for all P above p for all primes up to bound
   
   return true;
 }
 
+// This function checks whether the L-polynomial is reducible (indicating that the form is a lift)
+// and returns the corresponding automorphic type.
+// The relevant form is indicated by ev_idx (so f is the form in that index in evs)
+// p is the prime at which to compute the L-polynomial,
+// c is the conductor
+// and genus is the genus
 aut_type ev_is_lpoly_reducible(const eigenvalues_t evs, slong ev_idx, slong p, slong c, const genus_t genus)
 {
   int k;
@@ -275,7 +339,9 @@ aut_type ev_is_lpoly_reducible(const eigenvalues_t evs, slong ev_idx, slong p, s
   nf_elem_t root;
   bool is_lift;
   aut_type lift_type;
-  
+
+  // Since we are in rank 5, there are only two Hecke operators T_{p,1} and T_{p,2} at every prime.
+  // We store the Hecke eigenvalues of f for these operators
   for (k = 1; k < 3; k++) {
     get_hecke_ev_nbr_data_all_conductors(a[k-1], genus, evs, p, k, ev_idx, c);
   }
@@ -287,6 +353,10 @@ aut_type ev_is_lpoly_reducible(const eigenvalues_t evs, slong ev_idx, slong p, s
     printf("\n");
   }
 #endif // DEBUG
+
+  // It appears that the reduciblity of the L-polynomial is controlled
+  // by a quadratic equation, so one has only to check whether
+  // a certain discriminant is a square
   
   nf_elem_init(disc, evs->nfs[ev_idx]);
 
@@ -310,6 +380,7 @@ aut_type ev_is_lpoly_reducible(const eigenvalues_t evs, slong ev_idx, slong p, s
   is_lift = nf_elem_is_square_fast(disc, evs->nfs[ev_idx]);
   assert(is_lift == nf_elem_is_square(disc, evs->nfs[ev_idx]));
 
+  // If is a lift setting it to a Yoshida lift temporarily, otherwise type G.
   lift_type = is_lift ? Y : G;
 
   // if a lift, find which type of lift
@@ -336,6 +407,7 @@ aut_type ev_is_lpoly_reducible(const eigenvalues_t evs, slong ev_idx, slong p, s
   return lift_type;
 }
 
+// Setting lift types for all eigenforms
 void eigenvalues_set_lifts(eigenvalues_t evs, slong prec, slong c, const genus_t genus)
 {
   slong cnt, i;
@@ -345,12 +417,16 @@ void eigenvalues_set_lifts(eigenvalues_t evs, slong prec, slong c, const genus_t
   fmpz_init_set_si(p,2); // p = 2
   
   for (cnt = 0; cnt < prec; cnt++) {
+    // skipping bad primes
     while (fmpz_divisible(genus->disc, p))
       fmpz_nextprime(p, p, true);
     for (i = 0; i < evs->num; i++) {
+      // If this is an oldform, we don't try to further analyze
       if (evs->lift_type[i] == O)
 	continue;
       lift_type = ev_is_lpoly_reducible(evs, i, fmpz_get_si(p), c, genus);
+      // If this is the first pass, initialize according to result.
+      // If we get a different lift type at different primes then it cannot be a lift.
       if (cnt == 0) 
 	evs->lift_type[i] = lift_type;
       else
@@ -365,6 +441,7 @@ void eigenvalues_set_lifts(eigenvalues_t evs, slong prec, slong c, const genus_t
   return;
 }
 
+// Get the pivot for an eigenvector
 slong ev_get_pivot(const nf_elem_t* evec, const nf_t K, const genus_t genus, slong cond)
 {
   slong pivot, i, max_aut;
@@ -372,7 +449,9 @@ slong ev_get_pivot(const nf_elem_t* evec, const nf_t K, const genus_t genus, slo
   max_aut = 1;
   pivot = -1;
   for (i = 0; i < genus->dims[cond]; i++) {
+    // Look for a nonzero entry
     if (!nf_elem_is_zero(evec[i], K)) {
+      // with maximal number of automorphisms
       if (max_aut < genus->num_auts[cond][i]) {
 	max_aut = genus->num_auts[cond][i];
 	pivot = i;
