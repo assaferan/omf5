c233cfc3056fb7c3a3b4002ece27e2b294c119df
assaferan <assaferan@gmail.com>
2023-06-16 18:41:07 -0400

    updated documentation

--- include/fq_nmod_mpoly.h
+++ include/fq_nmod_mpoly.h
@@ -1,9 +1,59 @@
+/*************************************************************
+ *
+ * Package : omf5 - orthogonal modular forms of rank 5
+ * Filename : fq_nmod_mpoly.h
+ *
+ * Description: functions for handling the type fq_nmod_mpoly
+ *              from FLINT, multivariable polynomials
+ *              over finite fields.
+ *
+ *************************************************************
+ */
+
 #ifndef __FQ_NMOD_MPOLY_H__
 #define __FQ_NMOD_MPOLY_H__
 
-#include "flint/fq_nmod_mpoly.h"
+// Required packages dependencies
+
+#include <flint/fq_nmod_mpoly.h>
+
+/*******************************************************************
+ *
+ * Function: fq_nmod_mpoly_quadratic_part
+ *
+ * Description: Get the degree 2 homogenous component of
+ *              a polynomial f.
+ *
+ * Arguments:
+ *     + f (const fq_nmod_mpoly_t) - the polynomial
+ *     + F (const fq_nmod_ctx_t) - the finite field
+ *
+ * Returns:
+ *     + quad (fq_nmod_mpoly_t) - the quadratic part of f
+ *
+ ******************************************************************
+ */
 
 void fq_nmod_mpoly_quadratic_part(fq_nmod_mpoly_t quad, const fq_nmod_mpoly_t f, const fq_nmod_mpoly_ctx_t ctx);
+
+/*******************************************************************
+ *
+ * Function: fq_nmod_mpoly_linear_part
+ *
+ * Description: Get the degree 1 homogenous component of
+ *              a polynomial f.
+ *
+ * Arguments:
+ *     + f (const fq_nmod_mpoly_t) - the polynomial
+ *     + F (const fq_nmod_ctx_t) - the finite field
+ *
+ * Returns:
+ *     + lin (fq_nmod_mat_t) - a row vector representing the linear
+ *                             component of f. 
+ *
+ ******************************************************************
+ */
+
 void fq_nmod_mpoly_linear_part(fq_nmod_mat_t lin, const fq_nmod_mpoly_t f, const fq_nmod_mpoly_ctx_t ctx);
 
 #endif // __FQ_NMOD_MPOLY_H__
--- src/fq_nmod_mpoly.c
+++ src/fq_nmod_mpoly.c
@@ -1,5 +1,20 @@
+/*************************************************************
+ *
+ * Package : omf5 - orthogonal modular forms of rank 5
+ * Filename : fq_nmod_mpoly.c
+ *
+ * Description: functions for handling the type fq_nmod_mpoly
+ *              from FLINT, multivariable polynomials
+ *              over finite fields.
+ *
+ *************************************************************
+ */
+
+// Self dependencies
+
 #include "fq_nmod_mpoly.h"
 
+// Get the degree 2 homogeneous part of the polynomial
 void fq_nmod_mpoly_quadratic_part(fq_nmod_mpoly_t quad, const fq_nmod_mpoly_t f, const fq_nmod_mpoly_ctx_t ctx)
 {
   slong i, j;
@@ -12,12 +27,14 @@ void fq_nmod_mpoly_quadratic_part(fq_nmod_mpoly_t quad, const fq_nmod_mpoly_t f,
   // !! TODO - maube use get_term and get_exp instead
   fq_nmod_init(coeff, ctx->fqctx);
   fq_nmod_mpoly_zero(quad, ctx);
-  
+
+  // we just loop over the monomials
   for (i = 0; i < f->length; i++) {
     fq_nmod_mpoly_get_term_exp_ui(exp, f, i, ctx);
     deg = 0;
     for (j = 0; j < ctx->minfo->nvars; j++)
       deg += exp[j];
+    // if the degree of the monomial is 2, we add it (multiplied by its coefficient) to the result
     if (deg == 2) {
       fq_nmod_mpoly_get_coeff_fq_nmod_ui(coeff, f, exp, ctx);
       fq_nmod_mpoly_set_coeff_fq_nmod_ui(quad, coeff, exp, ctx);
@@ -28,6 +45,7 @@ void fq_nmod_mpoly_quadratic_part(fq_nmod_mpoly_t quad, const fq_nmod_mpoly_t f,
   return;
 }
 
+// Get the degree 1 homogeneous part of the polynomial, as a row vector
 void fq_nmod_mpoly_linear_part(fq_nmod_mat_t lin, const fq_nmod_mpoly_t f, const fq_nmod_mpoly_ctx_t ctx)
 {
   slong i, nvars;
@@ -40,6 +58,7 @@ void fq_nmod_mpoly_linear_part(fq_nmod_mat_t lin, const fq_nmod_mpoly_t f, const
     exp[i] = 0;
 
   for (i = 0; i < nvars; i++) {
+    // set the exponent to be e_i, so we get the coefficient of x_i
     exp[i] = 1;
     if (i >= 1) exp[i-1] = 0;
     fq_nmod_mpoly_get_coeff_fq_nmod_ui(fq_nmod_mat_entry(lin, 0, i), f, exp, ctx);
