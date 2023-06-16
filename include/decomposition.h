9a4e4e3fb612b410358f8e657da75699e7f60232
assaferan <assaferan@gmail.com>
2023-06-16 16:59:55 -0400

    updated documentation

--- include/decomposition.h
+++ include/decomposition.h
@@ -1,11 +1,50 @@
+/**********************************************************
+ *
+ * Package : omf5 - orthogonal modular forms of rank 5
+ * Filename : decomposition.h
+ *
+ * Description: data of Hecke module decomposition
+ *
+ **********************************************************
+ */
+
 #ifndef __DECOMPOSITION_H__
 #define __DECOMPOSITION_H__
 
+// Required packages dependencies
+
 #include <flint/fmpq_mat.h>
+
+// Self dependencies
 
 #include "genus.h"
 
-// this struct holds data for the decomposition of the spaces as Hecke modules
+/****************************************************************
+ *
+ * Type: decomposition_t
+ *
+ * Description: this struct holds data for the decomposition
+ *              of the spaces as Hecke modules.
+ *
+ * Fields:
+ *     + bases (fmpq_mat_t**) - a list of matrices
+ *                              for each conductor,
+ *                              the rows of each matrix
+ *                              form the basis for a submodule
+ *     + hecke (fmpq_mat_t**) - a list of Hecke operators
+ *                              for each prime
+ *     + num (slong*) - number of submodules in the decomposition
+ *                      for each conductor
+                        (num[c] is the size of bases[c]) 
+ *     + num_primes (slong) - number of primes for which we
+ *                            computed Hecke operators.
+ *                            (size of hecke)
+ *     + num_conductors (slong) - number of conductors
+ *                               (size of bases)
+ *
+ * Used to hold the data of a decomposition to Hecke submodules.
+ ****************************************************************
+ */
 
 typedef struct {
   fmpq_mat_t** bases; // bases for the vector spaces, by conductor
@@ -17,8 +56,59 @@ typedef struct {
 
 typedef decomposition_struct decomposition_t[1];
 
+/******************************************************************
+ *
+ * Function: decomposition_init
+ *
+ * Description: Allocates memory for a decomposition struct.
+ *
+ * Arguments:
+ *     + num (slong) - number of different conductors (spaces)
+ *
+ * Returns:
+ *     + decomp (decomposition_t) - the empty decomposition data
+ *
+ ******************************************************************
+ */
+
 void decomposition_init(decomposition_t decomp, slong num);
+
+/******************************************************************
+ *
+ * Function: decomposition_clear
+ *
+ * Description: Deallocates memory for a decomposition struct.
+ *
+ * Arguments:
+ *     + decomp (decomposition_t) - the decomposition data
+ *
+ ******************************************************************
+ */
+
 void decomposition_clear(decomposition_t decomp);
+
+/******************************************************************
+ *
+ * Function: decompose
+ *
+ * Description: Decomposes the space of orthogonal modular forms
+ *              of trivial weight for a specific genus,
+ *              and conductor c, specifying the appropriate
+ *              spinor norm twist.
+ *
+ * Arguments:
+ *     + genus (const genus_t) - the genus on which the space
+ *                               of modular forms is supported
+ *     + c (slong) - the conductor
+ *
+ * Returns:
+ *     + decomp (decomposition_t) - the full decomposition data
+ *                                  including bases for all
+ *                                  irreducible Hecke submodules
+ *
+ ******************************************************************
+ */
+
 void decompose(decomposition_t decomp, const genus_t genus, slong c);
 
 #endif // __DECOMPOSITION_H__
--- src/decomposition.c
+++ src/decomposition.c
@@ -1,5 +1,22 @@
+/**********************************************************
+ *
+ * Package : omf5 - orthogonal modular forms of rank 5
+ * Filename : decomposition.c
+ *
+ * Description: Decomposing M(Lambda) as a Hecke module
+ *
+ **********************************************************
+ */
+
+// System dependencies
+
 #include <assert.h>
+
+// Required packages dependencies
+
 #include <flint/fmpq_mat.h>
+
+// Self dependencies
 
 #include "arith.h"
 #include "decomposition.h"
@@ -7,11 +24,12 @@
 #include "genus.h"
 #include "hecke.h"
 
+// initialization
 void decomposition_init(decomposition_t decomp, slong num)
 {
   decomp->num = (slong*)malloc(num*sizeof(slong));
   decomp->bases = (fmpq_mat_t**)malloc(num*sizeof(fmpq_mat_t*));
-  // we shouold have num_primes here!!
+  // we should have num_primes here!!
   decomp->hecke = (fmpq_mat_t**)malloc(0*sizeof(fmpq_mat_t*));
   decomp->num_primes = 0;
   decomp->num_conductors = num;
@@ -19,6 +37,18 @@ void decomposition_init(decomposition_t decomp, slong num)
   return;
 }
 
+// This function tries to decompose a subspace fixed by the Hecke algebra to irreducible subspaces,
+// using a finite list of Hecke operators -
+// Here basis_V is a matrix whose rows specify a basis for the subspace,
+// ps is a list of primes, whose associated Hecke operators we wish to use,
+// idx is the index of the prime we are currently using to decompose the space
+// num_ps is the number of primes in ps,
+// and c specifies the conductor
+//
+// The function proceeds recursively.
+// It returns true if all the subspaces are irreducible as Hecke modules,
+// and false if using all the primes in ps, there are still submodules which might be reducible.
+// (due to failure of multiplicity one)
 // TODO - get rid of the unnecessary recursion
 bool decomposition_finite_subspace(decomposition_t decomp, const genus_t genus, const fmpq_mat_t basis_V,
 				   const int* ps, slong idx, slong num_ps, slong c)
@@ -33,8 +63,11 @@ bool decomposition_finite_subspace(decomposition_t decomp, const genus_t genus, 
   if (dim_V == 0) {
     return true;
   }
-  
+
+  // If the index points beyond the length of our list of primes, we have failed to completely decompose the space
+  // using these primes.
   if (idx >= num_ps) {
+    // Wrapping up what we did compute - add the resulting basis to the appropriate place in decomp
     (decomp->num[c])++;
     decomp->bases[c] = (fmpq_mat_t*)realloc(decomp->bases[c], (decomp->num[c])*sizeof(fmpq_mat_t));
     fmpq_mat_init(decomp->bases[c][decomp->num[c]-1], dim_V, fmpq_mat_ncols(basis_V));
@@ -55,6 +88,10 @@ bool decomposition_finite_subspace(decomposition_t decomp, const genus_t genus, 
   fprintf(stderr, "idx = %ld\n",idx);
   fprintf(stderr, "decomp->hecke[idx] = 0x%lx\n", (unsigned long)decomp->hecke[idx]);
 #endif // DEBUG_LEVEL_FULL
+
+  // If we have not yet computed the Hecke operators for this prime, we compute them now.
+  // Note that with current implementation, computing the Hecke operator for all conductors at once saves time,
+  // hence we compute already for all of them.
   if (idx >= decomp->num_primes) {
 #ifdef DEBUG
     fprintf(stderr, "computing more hecke operators\n");
@@ -69,12 +106,15 @@ bool decomposition_finite_subspace(decomposition_t decomp, const genus_t genus, 
   fmpq_mat_print(decomp->hecke[idx][c]);
   fprintf(stderr, "restricting hecke operator to space\n");
 #endif // DEBUG
+  // T is the restriction of the Hecke operator to the relevant subspace
   restrict_mat(T, decomp->hecke[idx][c], basis_V);
 #ifdef DEBUG
   fprintf(stderr, "restricted matrix is");
   fmpq_mat_print(T);
 #endif // DEBUG
-  
+
+  // We factor the characteristic polynomials to obtain the subspaces
+  // which are stable by its action
   fmpq_mat_charpoly(f, T);
   fmpq_poly_factor(fac, f);
 
@@ -82,6 +122,7 @@ bool decomposition_finite_subspace(decomposition_t decomp, const genus_t genus, 
   for (i = 0; i < fac->num; i++) {
     fmpq_poly_evaluate_fmpq_mat(fT, &(fac->p[i]), T);
     kernel_on(W, fT, basis_V);
+    // W is the stable subspace corresponding to this irreducible factor 
     assert(W->rows != 0);
     // We lift the basis to vectors of the original space
     // fmpq_mat_mul(W, W, basis_V);
@@ -92,7 +133,7 @@ bool decomposition_finite_subspace(decomposition_t decomp, const genus_t genus, 
       fmpq_mat_init_set(decomp->bases[c][decomp->num[c]-1], W);
       is_complete_W = true;
     }
-    else {
+    else { // if W = V is not irreducible, we move on to the next prime. If W != V, we can recursively decompose W.
       next_idx = (fmpq_mat_nrows(W) == dim_V) ? idx+1 : 0;
       is_complete_W = decomposition_finite_subspace(decomp, genus, W, ps, next_idx, num_ps, c);
       
@@ -109,6 +150,15 @@ bool decomposition_finite_subspace(decomposition_t decomp, const genus_t genus, 
   return is_complete;
 }
 
+// This function tries to decompose the space of orthogonal modular forms
+// using a finite list of primes, specifiying the relevant Hecke operators -
+// genus is the genus of the lattice,
+// ps is the list of primes,
+// num_ps their number,
+// and c specifies the conductor (twist of the spinor norm)
+// It returns true if all the subspaces in the decomposition are irreducible Hecke modules,
+// and false if after using all primes there are still modules which mght be reducible.
+// The decomposition is stored in decomp.
 bool decomposition_finite(decomposition_t decomp, const genus_t genus, const int* ps,
 			  slong num_ps, slong c)
 {
@@ -127,12 +177,23 @@ bool decomposition_finite(decomposition_t decomp, const genus_t genus, const int
   fmpq_mat_init(basis_M, dim, dim);
   fmpq_mat_one(basis_M);
 
+  // We use the previous recursive function, starting at the first prime, and with our
+  // subspace being the entire space
   is_complete = decomposition_finite_subspace(decomp, genus, basis_M, ps, 0, num_ps, c);
   
   fmpq_mat_clear(basis_M);
   
   return is_complete;
 }
+
+// Finally, we may use the decomposition bases on a finite list of primes
+// to write a function that fully decomposes the space.
+// In theory, we would like to proceed until the decomposition is complete.
+// However, due to oldforms, we may have a failure of multiplicity one,
+// resulting in an infinite recursion.
+// Until we implement degeneracy maps to overcome this issue,
+// we fix a bound on the primes used, whcih should be sufficient for
+// our purposes.
 
 void decompose(decomposition_t decomp, const genus_t genus, slong c)
 {
@@ -158,6 +219,7 @@ void decompose(decomposition_t decomp, const genus_t genus, slong c)
   return;
 }
 
+// clear memory allocared for the decomposition structure
 void decomposition_clear(decomposition_t decomp)
 {
   slong c, i;
