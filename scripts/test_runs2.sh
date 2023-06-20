3dcebd71dbf221fe4c292ade8b8d71141f2d4ae2
assaferan <assaferan@gmail.com>
2023-06-20 12:18:36 -0400

    adding list of nonlifts and modified test runs

---
+++ scripts/test_runs2.sh
@@ -0,0 +1,51 @@
+export LD_LIBRARY_PATH=/home/assaferan/lib
+
+./src/omf5 -format=GG -quad=1,0,0,1,1,1,0,1,0,1,0,0,1,0,8 -p=31 -hecke -row >& ./logs/omf5_61_31.log &
+./src/omf5 -format=GG -quad=1,0,0,1,1,1,0,1,0,1,0,0,1,0,8 -p=41 -hecke -row >& ./logs/omf5_61_41.log &
+./src/omf5 -format=GG -quad=1,0,0,1,1,1,0,1,0,1,0,0,1,0,8 -p=101 -hecke -row >& ./logs/omf5_61_101.log &
+./src/omf5 -format=GG -quad=1,0,0,1,1,1,0,1,0,1,0,0,1,0,8 -p=127 -hecke -row >& ./logs/omf5_61_127.log &
+./src/omf5 -format=GG -quad=1,0,0,1,1,1,0,1,0,1,0,0,1,0,8 -p=199 -hecke -row >& ./logs/omf5_61_199.log &
+./src/omf5 -format=GG -quad=1,0,0,1,1,1,0,1,0,1,0,0,1,0,8 -p=293 -hecke -row >& ./logs/omf5_61_293.log &
+./src/omf5 -format=GG -quad=1,0,0,1,1,1,0,1,0,1,0,0,1,0,8 -p=397 -hecke -row >& ./logs/omf5_61_397.log &
+
+./src/omf5 -quad=1,1,0,1,0,1,0,0,0,1,1,0,1,1,34 -format=GG -p=31 -hecke -row >& ./logs/omf5_167_31.log &
+./src/omf5 -quad=1,1,0,1,0,1,0,0,0,1,1,0,1,1,34 -format=GG -p=41 -hecke -row >& ./logs/omf5_167_41.log &
+./src/omf5 -quad=1,1,0,1,0,1,0,0,0,1,1,0,1,1,34 -format=GG -p=101 -hecke -row >& ./logs/omf5_167_101.log &
+./src/omf5 -quad=1,1,0,1,0,1,0,0,0,1,1,0,1,1,34 -format=GG -p=127 -hecke -row >& ./logs/omf5_167_127.log &
+./src/omf5 -quad=1,1,0,1,0,1,0,0,0,1,1,0,1,1,34 -format=GG -p=199 -hecke -row >& ./logs/omf5_167_199.log &
+
+./src/omf5 -quad=1,0,0,1,0,1,0,1,0,1,0,1,1,0,33 -format=GG -p=31 -hecke -row >& ./logs/omf5_262_31.log &
+./src/omf5 -quad=1,0,0,1,0,1,0,1,0,1,0,1,1,0,33 -format=GG -p=41 -hecke -row >& ./logs/omf5_262_41.log &
+./src/omf5 -quad=1,0,0,1,0,1,0,1,0,1,0,1,1,0,33 -format=GG -p=101 -hecke -row >& ./logs/omf5_262_101.log &
+./src/omf5 -quad=1,0,0,1,0,1,0,1,0,1,0,1,1,0,33 -format=GG -p=127 -hecke -row >& ./logs/omf5_262_127.log &
+./src/omf5 -quad=1,0,0,1,0,1,0,1,0,1,0,1,1,0,33 -format=GG -p=199 -hecke -row >& ./logs/omf5_262_199.log &
+
+./src/omf5 -quad=1,0,0,1,0,1,0,1,0,1,1,1,2,0,17 -format=GG -p=31 -hecke -row >& ./logs/omf5_334_31.log &
+./src/omf5 -quad=1,0,0,1,0,1,0,1,0,1,1,1,2,0,17 -format=GG -p=41 -hecke -row >& ./logs/omf5_334_41.log &
+./src/omf5 -quad=1,0,0,1,0,1,0,1,0,1,1,1,2,0,17 -format=GG -p=101 -hecke -row >& ./logs/omf5_334_101.log &
+./src/omf5 -quad=1,0,0,1,0,1,0,1,0,1,1,1,2,0,17 -format=GG -p=127 -hecke -row >& ./logs/omf5_334_127.log &
+./src/omf5 -quad=1,0,0,1,0,1,0,1,0,1,1,1,2,0,17 -format=GG -p=199 -hecke -row >& ./logs/omf5_334_199.log &
+
+./src/omf5 -quad=1,0,0,1,0,1,0,0,1,1,0,1,1,0,42 -format=GG -p=31 -hecke -row >& ./logs/omf5_498_31.log &
+./src/omf5 -quad=1,0,0,1,0,1,0,0,1,1,0,1,1,0,42 -format=GG -p=41 -hecke -row >& ./logs/omf5_498_41.log &
+./src/omf5 -quad=1,0,0,1,0,1,0,0,1,1,0,1,1,0,42 -format=GG -p=101 -hecke -row >& ./logs/omf5_498_101.log &
+./src/omf5 -quad=1,0,0,1,0,1,0,0,1,1,0,1,1,0,42 -format=GG -p=127 -hecke -row >& ./logs/omf5_498_127.log &
+./src/omf5 -quad=1,0,0,1,0,1,0,0,1,1,0,1,1,0,42 -format=GG -p=199 -hecke -row >& ./logs/omf5_498_199.log &
+
+./src/omf5 -quad=1,1,0,1,0,1,0,0,0,1,1,1,1,0,101 -format=GG -p=31 -hecke -row >& ./logs/omf5_503_31.log &
+./src/omf5 -quad=1,1,0,1,0,1,0,0,0,1,1,1,1,0,101 -format=GG -p=41 -hecke -row >& ./logs/omf5_503_41.log &
+./src/omf5 -quad=1,1,0,1,0,1,0,0,0,1,1,1,1,0,101 -format=GG -p=101 -hecke -row >& ./logs/omf5_503_101.log &
+./src/omf5 -quad=1,1,0,1,0,1,0,0,0,1,1,1,1,0,101 -format=GG -p=127 -hecke -row >& ./logs/omf5_503_127.log &
+./src/omf5 -quad=1,1,0,1,0,1,0,0,0,1,1,1,1,0,101 -format=GG -p=199 -hecke -row >& ./logs/omf5_503_199.log &
+
+./src/omf5 -format=GG -quad=1,-1,1,-1,-1,2,-2,1,1,2,-2,-2,2,1,18 -p=31 -hecke -row >& ./logs/omf5_993_31.log &
+./src/omf5 -format=GG -quad=1,-1,1,-1,-1,2,-2,1,1,2,-2,-2,2,1,18 -p=41 -hecke -row >& ./logs/omf5_993_41.log &
+./src/omf5 -format=GG -quad=1,-1,1,-1,-1,2,-2,1,1,2,-2,-2,2,1,18 -p=101 -hecke -row >& ./logs/omf5_993_101.log &
+./src/omf5 -format=GG -quad=1,-1,1,-1,-1,2,-2,1,1,2,-2,-2,2,1,18 -p=127 -hecke -row >& ./logs/omf5_993_127.log &
+./src/omf5 -format=GG -quad=1,-1,1,-1,-1,2,-2,1,1,2,-2,-2,2,1,18 -p=199 -hecke -row >& ./logs/omf5_993_199.log &
+
+./src/omf5 -quad=1,1,1,1,1,1,1,1,1,2,-1,0,11,7,11 -format=GG -hecke -p=31 -row >& ./logs/omf5_1993_31.log &
+./src/omf5 -quad=1,1,1,1,1,1,1,1,1,2,-1,0,11,7,11 -format=GG -hecke -p=41 -row >& ./logs/omf5_1993_41.log &
+./src/omf5 -quad=1,1,1,1,1,1,1,1,1,2,-1,0,11,7,11 -format=GG -hecke -p=101 -row >& ./logs/omf5_1993_101.log &
+./src/omf5 -quad=1,1,1,1,1,1,1,1,1,2,-1,0,11,7,11 -format=GG -hecke -p=127 -row >& ./logs/omf5_1993_127.log &
+./src/omf5 -quad=1,1,1,1,1,1,1,1,1,2,-1,0,11,7,11 -format=GG -hecke -p=199 -row >& ./logs/omf5_1993_199.log &
